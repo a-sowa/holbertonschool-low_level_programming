8e61f3960a2173a3e64c18868ff145c513c6dc10
Axel Sowa <axelsowapro@gmail.com>
2023-10-16 05:58:02 -0700

    C program - Prints digit numbers of base 10 starting from 0

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Main function: prints digit number of base 10 starting from 0
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int nb = 0;
+
+	while (nb < 10)
+	{
+	printf("%d", nb);
+	nb++;
+	}
+	printf("\n");
+
+	return (0);
+}
