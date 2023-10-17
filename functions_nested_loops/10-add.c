41b3d482ddda3e8b3007432df6797338c9098d2c
Axel Sowa <axelsowapro@gmail.com>
2023-10-17 09:04:34 -0700

    Add C program - Func adding two integers and returning value (sum)

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * add func: adds  two integer and return the value of sum
+ * @nb1: input integer
+ * @nb2: input integer
+ * Return: sum value (Success)
+ */
+
+int add(int nb1, int nb2)
+{
+	int sum = nb1 + nb2;
+
+	return (sum);
+}
