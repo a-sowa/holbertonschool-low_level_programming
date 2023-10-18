7490e73177453fce0e8bfa1b7b4211f976b1666a
Axel Sowa <axelsowapro@gmail.com>
2023-10-18 08:52:14 -0700

    Add C program - Func returning 1 if input char is upper

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ * _isupper func: Return 1 if parameter c is upper character
+ * @c: input character
+ * Return: 1 if c is upper
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
