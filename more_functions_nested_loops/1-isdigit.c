2a7c3ba0e4ee6352116291b0213c0cb344a1dd19
Axel Sowa <axelsowapro@gmail.com>
2023-10-18 08:57:59 -0700

    Add C program - Func returning 1 if input is a digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * _isdigit func: Return 1 if parameter c is a digit
+ * @c: input character
+ * Return: 1 if c is a digit
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
