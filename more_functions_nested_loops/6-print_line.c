0ea91b1d8ca8054ff107ac5712e878595d622281
Axel Sowa <axelsowapro@gmail.com>
2023-10-18 11:48:17 -0700

    Add C program - prints a line according to the number given in parameter

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ * print_line func: prints a line according to the number given in parameter
+ * @n: input number
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+		_putchar('\n');
+	} else
+		_putchar('\n');
+}
