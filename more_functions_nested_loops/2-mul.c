9e8240c206dd89ad2ab439dc0c19b3252e7ce516
Axel Sowa <axelsowapro@gmail.com>
2023-10-18 09:04:54 -0700

    Add C program - Func that multiplies 2 int

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * mul func: multiply two integers and return the value
+ * @a: input integer
+ * @b: input integer
+ * Return: result value (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
