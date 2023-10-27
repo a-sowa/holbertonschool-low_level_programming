a785e86859e1b22aa1888b52536fff74b4a44634
Axel Sowa <axelsowapro@gmail.com>
2023-10-27 02:44:26 -0700

    Func that locates a substring

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -26,4 +26,5 @@ char *_strchr(char *s, char c);
 char *leet(char *);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
 #endif
