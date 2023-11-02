e65c3c8e160f1b056289698b817c6bc26d235d2a
Axel Sowa <axelsowapro@gmail.com>
2023-11-02 05:53:38 -0700

    C program that prints its name using argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point - prints naame of the program
+ * @argc: number of arguments
+ * @argv: array of arguments pointers
+ * Return: 0 (success)
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
