d18c9d4fd93f9611760442f44403949da158c162
a-sowa <axelsowapro@gmail.com>
2023-11-15 19:40:53 +0000

    Adding header file with new dog structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+  * struct dog - Dog's attributes
+  * @name: Dog's name
+  * @age: Dog's age
+  * @owner: Dog's owner
+  */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
