8eede1614565106757e513f23b0a0eeadee9cfd3
a-sowa <axelsowapro@gmail.com>
2024-01-17 16:57:47 +0000

    Adding comma as separator

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -28,7 +28,11 @@ int binary_search(int *array, size_t size, int value)
 
 		for (i = left; i <= right; i++)
 		{
-			printf("%d ", array[i]);
+			printf("%d", array[i]);
+			if (i < right)
+			{
+				printf(", ");
+			}
 		}
 		printf("\n");
 
